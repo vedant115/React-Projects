1fb31e8fbaeb0a2efc735f25dcf5a593d61f2575
unknown <vedant.a.rahane@gmail.com>
2022-10-31 11:51:54 +0530

    note todo list project added

---
+++ note-todo-list/build/precache-manifest.34572fb30c0f0623855fe8f10636b4f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae35c27c63d73ca2ea7ae01ed700af67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "ba093a34e188fe065cde",
+    "url": "/static/js/main.465b3e08.chunk.js"
+  },
+  {
+    "revision": "db65393590a7b75d6022",
+    "url": "/static/js/runtime-main.f8e38478.js"
+  }
+]);
